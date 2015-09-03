1a2c447be7fc1d57d16073854eb408e721c053a9
Pasi Ahopelto <pahopelt@Pasis-MacBook-Pro.local>
2015-09-03 19:35:36 +0300

    Added elevation to geolocation page.

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,23 +1,32 @@
+var mapPanel = document.getElementById("mapPanel");
+
 function updateLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(updateMap)
     } else {
-        coordinatesPanel.innerHTML = "Geolocation not supported."
+        mapPanel.innerHTML = "Geolocation not supported."
     }
 }
 
 function updateMap(position) {
-    var map = new google.maps.Map(mapPanel, { 
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+    var infoWindow = new google.maps.InfoWindow({
+        map: new google.maps.Map(mapPanel, { 
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+    	}),
+    	position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        content: 'Getting elevation...'
     });
-    var geoLocator = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    var infowindow = new google.maps.InfoWindow({
-        map: map,
-        position: geoLocator,
-        content: 'You are here'
+    infoWindow.map.setCenter(infoWindow.position);
+    new google.maps.ElevationService().getElevationForLocations( { 'locations': [ infoWindow.position ] }, function (results, status) {
+	var elevation = 'You are at unknown elevation';
+        if (status == google.maps.ElevationStatus.OK) {
+	    elevation = 'You may be at ' + Math.round(results[0].elevation) + ' m elevation.';
+	}
+	else {
+	    console.error('Failed to get elevation: ' + status);
+	}
+	infoWindow.setContent(elevation);
+	infoWindow.open(infoWindow.map);
     });
-    map.setCenter(geoLocator);
 }
-
-var mapPanel = document.getElementById("mapPanel");
